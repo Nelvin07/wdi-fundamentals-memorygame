13303706ae9457351e27f1fee624cc19e61121af
Nelvin <nellysingh07@gmail.com>
2018-05-22 18:42:27 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,30 @@
 console.log("Up and running!");
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+var cards = ["Queen", "Queen", "King", "King"];
+
+var cardsInPlay = []
+
+var cardOne = cards[0];
+var cardTwo = cards[2];
+
+cardsInPlay.push("cardOne", "cardTwo");
+
+if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You have found a match");
+    } else {
+	alert("sorry, try again!");
+    }
+}
+
+console.log()
+
+console.log("User flipped" + cardOne);
+console.log("User flipped" + cardTwo);
+
+
+
+
+
+
+
